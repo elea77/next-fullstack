9b277183782106a4bfa627b77d7a17f9f08e2598
eleacarton <eleacarton@me.com>
2022-01-13 10:22:31 +0100

    update

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,7 +2,7 @@ import apiConfigs from "../../next.config.js";
 
 export default {
     register(user) {
-        return fetch(`${apiConfigs.env.API_URL}api/v1/users`, {
+        return fetch(`${apiConfigs.env.API_URL}users`, {
             method: "POST",
             headers: {
               "content-type": "application/json",
@@ -11,7 +11,7 @@ export default {
           }).then((res) => res.json())
     },
     login(user) {
-        return fetch(`${apiConfigs.env.API_URL}api/v1/login`, {
+        return fetch(`${apiConfigs.env.API_URL}login`, {
             method: "POST",
             headers: {
               "content-type": "application/json",
@@ -20,7 +20,7 @@ export default {
           }).then((res) => res.json())
     },
     getUser(token) {
-        return fetch(`${apiConfigs.env.API_URL}api/v1/users/get-user`, {
+        return fetch(`${apiConfigs.env.API_URL}users/get-user`, {
             headers: {
                 "authorization":token
             }
